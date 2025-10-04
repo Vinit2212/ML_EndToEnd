5f69a1977df1e8e0408497644870d9ad3ab6f9f3
Your NameVinit2212 <youremail@example.comvinitbhanushali99@gmail.com>
2025-10-04 12:55:27 -0400

    Added Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 import dill
@@ -24,14 +25,23 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
             model_name = list(models.keys())[i]
+
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
             logging.info(f'Training the model: {model_name}')
+            # model.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
@@ -42,6 +52,8 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
 
             report[model_name] = test_model_score
             logging.info(f'{model_name} - Train Score: {train_model_score}, Test Score: {test_model_score}')
+
+            # report[list(models.keys())[i]] = test_model_score
 
         return report
 
